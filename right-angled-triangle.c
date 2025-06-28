14ed2adba06cd705b9170648ea9f3a95d8cb90ec
VasianPeci <140719484+VasianPeci@users.noreply.github.com>
2025-06-28 11:41:18 +0200

    Update right-angled-triangle.c

--- right-angled-triangle.c
+++ right-angled-triangle.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
+
 int main(){
     int n,i,j,h;
-    printf("Write the max number of stars which will be displayed in the last row: ");
-    scanf("%d",&n);
-    for (i=0;i<n;i++){
-        for(h=1;h<=n-i-1;h++){
+    printf("Enter the max number of rows: ");
+    scanf("%d", &n);
+    for (i = 0; i < n; i++){
+        for(h = 1; h <= n-i-1; h++){
             printf(" ");
         }
-        for(j=1;j<=i+1;j++){
+        for(j = 1; j <= i+1; j++){
             printf("*");
         }
         printf("\n");
